756e73eb62a4bc9d466fdc0a270116cf53718de5
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-07-23 11:28:48 +0530

    added algorithm & output

--- 5/Triangle.java
+++ 5/Triangle.java
@@ -1,23 +1,47 @@
-import java.io.*;
-import java.lang.Math;
-import java.util.Scanner;
+// Check whether the triangle is equilateral, isosceles or scalene
 
-public class Armstrong
+// Algorithm
+/* 
+1. Start
+2. Read length of three sides of the triangle as 
+	s1, s2, s3 respectively
+3. if s1 = s2 and s2 = s3, 
+	print "Equilateral"
+4. else if s1 = s2 or s2 = s3 or s3 = s1,
+	print "Isosceles"
+5. else
+	print "Scalene"
+6. Stop
+*/
+
+import java.io.*;
+
+public class Triangle
 {
-	public static void main(String args[])
+	public static void main(String args[]) throws IOException
 	{
-		int s1, s2, s3;
-		Scanner s = new Scanner();
-		System.out.print("Enter the three sides of a triangle : ");
-		s1 = s.nextInt();
-		s2 = s.nextInt();
-		s3 = s.nextInt();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("Enter the three sides of a triangle : ");
+		int s1 = Integer.parseInt(in.readLine());
+		int s2 = Integer.parseInt(in.readLine());
+		int s3 = Integer.parseInt(in.readLine());
 		
 		if (s1 == s2  && s2 == s3)
-			System.out.println("The triangle is equilateral triangle. ");
+			System.out.println("\nThe triangle is equilateral triangle. ");
 		else if (s1 == s2 || s2 == s3 || s3 == s1)
-			System.out.println("The triangle is isosceles triangle. ");
+			System.out.println("\nThe triangle is isosceles triangle. ");
 		else 
-			System.out.println("The triangle is scalene. ");
+			System.out.println("\nThe triangle is scalene. ");
 	}
 }
+
+// Expected output
+/* 
+Enter the three sides of a triangle : 
+5
+6
+5
+
+The triangle is isosceles triangle.
+*/
