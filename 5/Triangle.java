40aa387a60de52cf5274277bfa31a7a6b0c934fc
ASHISH ANTONY PAULY <72491974+ashishantonytsr@users.noreply.github.com>
2022-07-23 10:30:20 +0530

    Added procedure to check triangle

--- 5/Triangle.java
+++ 5/Triangle.java
@@ -6,6 +6,18 @@ public class Armstrong
 {
 	public static void main(String args[])
 	{
-
+		int s1, s2, s3;
+		Scanner s = new Scanner();
+		System.out.print("Enter the three sides of a triangle : ");
+		s1 = s.nextInt();
+		s2 = s.nextInt();
+		s3 = s.nextInt();
+		
+		if (s1 == s2  && s2 == s3)
+			System.out.println("The triangle is equilateral triangle. ");
+		else if (s1 == s2 || s2 == s3 || s3 == s1)
+			System.out.println("The triangle is isosceles triangle. ");
+		else 
+			System.out.println("The triangle is scalene. ");
 	}
 }
