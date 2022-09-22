afbdce8298a4cb5e0b8ceb2f5a89f168a678fa08
~/antony/ashish <72491974+ashishantonytsr@users.noreply.github.com>
2022-09-22 06:54:42 +0530

    used foreach loops

--- SEM-05-JAVA/08/MergeTwoArrays.java
+++ SEM-05-JAVA/08/MergeTwoArrays.java
@@ -12,22 +12,26 @@ public class MergeTwoArrays
 		System.out.print("Enter the number of elements in first array : ");
 		int n1 = s.nextInt();
 		int first[] = new int[n1];
+
 		System.out.print("Enter the elements into first array : ");
-		for (int i=0; i<n1; i++) first[i] = s.nextInt();
+		for (int i = 0; i < n1; i++) first[i] = s.nextInt();
 
 		System.out.print("Enter the number of elements in second array : ");
 		int n2 = s.nextInt();
 		int second[] = new int[n2];
+
 		System.out.print("Enter the elements into second array : ");
-		for (int i=0; i<n2; i++) second[i] = s.nextInt();
+		for (int i = 0; i < n2; i++) second[i] = s.nextInt();
 
 		int result[] = new int[n1 + n2];
-		for (int i = 0; i < n1; i++){
-			result[i] = first[i];
-		}
-		for (int k = n1, i = 0; k < n1 + n2;){
-			result[k++] = second[i++];
-		}
+		int index = 0;
+		
+		for (int item : first)
+			result[index++] = item;
+
+		for (int item : second)
+			result[index++] = item;
+
 		Arrays.sort(result);
 
 		System.out.print("The merged sorted array is : ");
