9bee3140d5833a993693c24eb8eabe8f63d0cd85
~/antony/ashish <72491974+ashishantonytsr@users.noreply.github.com>
2022-10-11 19:52:41 +0530

    Added array to store marks of student

--- SEM-05-JAVA/15/Result.java
+++ SEM-05-JAVA/15/Result.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 class Student {
 	String name, course, regNo, grade;
-	int age, total = 0;
+	int age;
+	int marks[] = new int[5], total = 0;
 
 	void readStudentDetails() {
 		Scanner s = new Scanner(System.in);
 
-		System.out.println("Enter register number : ");
+		System.out.print("Enter register number : ");
 		this.regNo = s.nextLine();
-		System.out.println("Enter student name : ");
+		System.out.print("Enter student name : ");
 		this.name = s.nextLine();
-		System.out.println("Enter student age : ");
+		System.out.print("Enter student age : ");
 		this.age = Integer.parseInt(s.nextLine());
-		System.out.println("Enter course : ");
+		System.out.print("Enter course : ");
 		this.course = s.nextLine();
 	}
 
@@ -24,6 +25,8 @@ class Student {
 		System.out.println("Student Name : " + this.name);
 		System.out.println("Student Age : " + this.age);
 		System.out.println("Course : " + this.course);
+		for (int i = 0; i < 5; i++) 
+			System.out.printf("Subject %d : %d\n", i + 1, this.marks[i]);
 		System.out.println("Total Marks : " + this.total);
 		System.out.println("Student Grade : " + this.grade);
 	}
@@ -35,8 +38,10 @@ public class Result extends Student{
 		Scanner s = new Scanner(System.in);
 
 		System.out.println("Enter the mark of five subjects (out of 100) : ");
-		for (int i=0; i<5; i++)
-			super.total += s.nextInt();
+		for (int i = 0; i < 5; i++) {
+			super.marks[i] = s.nextInt();
+			total += super.marks[i];
+		}
 
 		double perc = super.total / 5;
 		super.grade = 
