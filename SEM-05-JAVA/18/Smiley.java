fe9f6e62687d9ad1eedc48390378c32c9a919243
~/antony/ashish <72491974+ashishantonytsr@users.noreply.github.com>
2022-11-03 18:59:47 +0530

    fixed minor bug

--- SEM-05-JAVA/18/Smiley.java
+++ SEM-05-JAVA/18/Smiley.java
@@ -6,12 +6,20 @@ import java.awt.*;
 </applet>
 */
 
-public class Smiley {
+public class Smiley extends Applet{
 	public void paint (Graphics g) {
 		g.drawOval(80, 70, 150, 150);
 		g.setColor(Color.BLACK);
 		g.fillOval(120, 120, 15, 15);
 		g.fillOval(170, 120, 15, 15);
-		g.drawArc(130, 180, 60, 20, 180, 180);
+		g.drawArc(130, 180, 50, 20, 180, 180);
 	}
 }
+
+
+
+
+/* 
+To compile with Java Version 8.0
+	javac --release 8 Smiley.java
+*/ 
