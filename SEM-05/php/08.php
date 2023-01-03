c1b8a674ea94b03a35f65e00e22c84f788728c29
~/antony/ashish <72491974+ashishantonytsr@users.noreply.github.com>
2023-01-03 22:20:26 +0530

    Updated ```08.php```

--- SEM-05/php/08.php
+++ SEM-05/php/08.php
@@ -3,27 +3,34 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Perfect Numbers</title>
+	<title>Perfect Number</title>
+	<link rel="stylesheet" href="./assets/css/html-01.css">
 </head>
 
 <center>
 <body>
-	<h2>Perfect Numbers</h2>
+	<h2>Perfect Number</h2>
 	<form action=" " method="POST" name="read_form">
-		<label for="limit">Enter the limit </label>
-		<input type="number" name="limit">
+		<label for="num">Enter the number </label>
+		<input type="number" name="num"><br>
 		<input type="submit" value="Submit">
 	</form>
 </body>
 
 <?php
 	if ($_POST) {
-		$lim = $_POST['limit'];
-
-		echo "<br>Perfect Numbers upto the limit are ";
-		for ($i = 1; $i <= $lim; $i++) {
-			echo $i * $i . " ";
+		$num = $_POST['num'];
+		$sum = 0;
+		for ($i = 1; $i < $num; $i++){
+			if ($num % $i == 0)
+				$sum += $i;
 		}
+		if ($sum > $num)
+		 echo "$num is an Abundant Number";
+		else if ($sum < $num)
+			echo "$num is a Deficient Number";
+		else
+			echo "$num is a Perfect Number";
 	}
 ?>
 </center>
