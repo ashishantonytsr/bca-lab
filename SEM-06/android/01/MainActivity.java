7dd5f18f94b0d82d22831855ec2f86aa13087165
~/antony/ashish <72491974+ashishantonytsr@users.noreply.github.com>
2023-01-21 20:01:35 +0530

    deleted unwanted ```java``` files

--- SEM-06/android/01/MainActivity.java
+++
@@ -1,8 +0,0 @@
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
--- SEM-06/android/04/MainActivity2.java
+++
@@ -1,8 +0,0 @@
-public class MainActivity2 extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-    }
-}
