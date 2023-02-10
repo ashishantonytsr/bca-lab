cfae4cf5a60abc6677067e2449ddd2fd852976b9
~/antony/ashish <72491974+ashishantonytsr@users.noreply.github.com>
2023-02-10 17:57:56 +0530

    Updations in ```shell-script``` ```14``` & ```15```
    Simplified pgrm ```14.sh```
    Added title & expected output; changed variable name; in pgrm ```15.sh```

--- SEM-06/linux-shell/14/14.sh
+++ SEM-06/linux-shell/14/14.sh
@@ -1,24 +1,18 @@
 # Gross Salary
 
 # !/bin/bash
-read -p "Enter the basic salary: " sal
+read -p "Enter the basic salary: " bsal
 
-if [ $sal -ge 1000 ]; then
-	DA=$(echo "scale=2; $sal*40/100" |bc )
-	HRA=$(echo "scale=2; $sal*20/100" |bc )
-	gsal=$(echo "scale=2; $sal+$DA+$HRA" |bc )
+if [ $bsal -ge 1000 ]; then
+	DA=$(( $bsal * 40/100 ))
+	HRA=$(( $bsal * 20/100 ))
+	gsal=$(( $bsal + $DA + $HRA ))
+	
+	echo "Gross salary is $gsal"
 fi
-
-echo "Basic salary is $sal"
-echo "DA is $DA"
-echo "HRA is $HRA"
-echo "Gross salary is $gsal"
 
 
 # Expected output
 # ---
-# Enter the basic salary: 2552
-# Basic salary is 2552
-# DA is 1020.80
-# HRA is 510.40
-# Gross salary is 4083.20
+# Enter the basic salary: 2500
+# Gross salary is 4000
--- SEM-06/linux-shell/15/15.sh
+++ SEM-06/linux-shell/15/15.sh
@@ -1,13 +1,20 @@
+# Greeting message based on the time of execution
+
 # !/bin/bash
+time=`date +"%H"`
 
-date=`date +"%H"`
-
-if [ $date -ge 9 ] && [ $date -lt 12 ]; then
+if [ $time -ge 9 ] && [ $time -lt 12 ]; then
 	echo "Good Morning"
-elif [ $date -ge 12 ] && [ $date -lt 16 ]; then
+elif [ $time -ge 12 ] && [ $time -lt 16 ]; then
 	echo "Good Afternoon"
-elif [ $date -ge 16 ] && [ $date -lt 20 ]; then
+elif [ $time -ge 16 ] && [ $time -lt 20 ]; then
 	echo "Good Evening"
 fi
 
 echo "The time is $(date +"%H:%M")"
+
+
+# Expected output
+# ---
+# Good Evening
+# The time is 17:01
