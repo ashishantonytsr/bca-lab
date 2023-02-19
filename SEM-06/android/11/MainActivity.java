44ab291477eda5a37a77601068be21e0a3287540
~/antony/ashish <72491974+ashishantonytsr@users.noreply.github.com>
2023-02-19 10:40:17 +0530

    Fixed intentation issues

--- SEM-06/android/11/MainActivity.java
+++ SEM-06/android/11/MainActivity.java
@@ -1,28 +1,31 @@
 public class MainActivity extends AppCompatActivity {
-    String[] spinnerItems = { "ANDROID", "PHP" };
+	Button btn;
+	Spinner spinner;
+	EditText editText;
+	String[] spinnerItems = { "ANDROID", "PHP" };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        Button btn = (Button) findViewById(R.id.btn);
+		spinner = (Spinner) findViewById(R.id.spinner);
+		editText = (EditText) findViewById(R.id.editText);
+		btn = (Button) findViewById(R.id.btn);
 
-        List stringList = new ArrayList(Arrays.asList(spinnerItems));
-        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, R.layout.spinner_item, stringList);
-        adapter.setDropDownViewResource(R.layout.spinner_item);
-        spinner.setAdapter(adapter);
-        
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String getText = editText.getText().toString();
-                stringList.add(getText);
-                adapter.notifyDataSetChanged();
-                Toast.makeText(MainActivity.this, "Item Added", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+		List stringList = new ArrayList(Arrays.asList(spinnerItems));
+		ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, R.layout.spinner_item, stringList);
+		adapter.setDropDownViewResource(R.layout.spinner_item);
+		spinner.setAdapter(adapter);
+
+		btn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				String getText = editText.getText().toString();
+				stringList.add(getText);
+				adapter.notifyDataSetChanged();
+				Toast.makeText(MainActivity.this, "Item Added", Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
 }
